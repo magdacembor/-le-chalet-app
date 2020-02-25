003c0f30cdcdebf5ed897691d69b2b99300d553c
Piotr Damer <piotrd@czuby.net>
2020-02-25 11:55:21 +0000

    ChaletsController and Chalet model validations/associations

--- app/models/chalet.rb
+++ app/models/chalet.rb
@@ -1,3 +1,13 @@
 class Chalet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :reviews
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
+  validates :country, presence: true
+  validates :address, presence: true, uniqueness: { scope: :country }
+  validates :number_of_guests, presence: true, numericality: { only_integer: true }
+  validates :number_of_bedrooms, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: true
+  validates :user_id, presence: true
 end
