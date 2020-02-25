3ca1b3222a6ec44cfbd070e2e1b2cfdb7bf6b424
Katrina Au <katrinaau95@gmail.com>
2020-02-25 10:58:40 +0000

    Add validation and associations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :chalet
+
+  validates :user_id, presence: true
+  validates :chalet_id, presence: true
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates_uniqueness_of :chalet_id, :scope => [:check_in, :check_out]
 end
