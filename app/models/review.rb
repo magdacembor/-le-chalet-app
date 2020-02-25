f791394e83e43369cebe45b429d096cd8be40cac
Alim Lalji <alimlalji1@gmail.com>
2020-02-25 11:37:01 +0000

    Validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :chalet
+
+  validates :content, :rating, :user, :chalet, presence: true
+  validates :rating, inclusion: { in: (1..5) }
+  validates :chalet, uniqueness: { scope: :user, message: "Only one review per user about this chalet" }
 end
