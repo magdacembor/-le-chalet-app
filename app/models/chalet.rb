9abaf8f4236ee29ce9439cafcdedb7638c1dddc3
Piotr Damer <piotrd@czuby.net>
2020-02-25 13:53:52 +0000

    Basic index, show, new and edit views

--- app/models/chalet.rb
+++ app/models/chalet.rb
@@ -4,7 +4,6 @@ class Chalet < ApplicationRecord
   has_many :reviews
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
-  validates :country, presence: true
   validates :address, presence: true, uniqueness: { scope: :country }
   validates :number_of_guests, presence: true, numericality: { only_integer: true }
   validates :number_of_bedrooms, presence: true, numericality: { only_integer: true }
