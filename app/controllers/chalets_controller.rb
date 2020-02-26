532c10e6644172832011efda78913952f43c215c
Alim Lalji <alimlalji1@gmail.com>
2020-02-26 14:21:31 +0000

    Added create reviews to chalet show page

--- app/controllers/chalets_controller.rb
+++ app/controllers/chalets_controller.rb
@@ -11,6 +11,8 @@ class ChaletsController < ApplicationController
   def show
     @booking = Booking.new
     @booking.user_id = current_user
+    @review = Review.new
+    @review.user_id = current_user
   end
 
   def new
