e71e918a67b876ec153b8a45aa6d9c6d2490e928
Magda Cembor <magda.cembor@gmail.com>
2020-02-27 15:44:35 +0000

    validation on no of guests

--- app/models/chalet.rb
+++ app/models/chalet.rb
@@ -8,6 +8,7 @@ class Chalet < ApplicationRecord
   validates :description, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: { scope: :country }
   validates :number_of_guests, presence: true, numericality: { only_integer: true }
+  validates :number_of_guests, numericality: { greater_than: 0 }
   validates :number_of_bedrooms, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: true
   validates :user_id, presence: true
