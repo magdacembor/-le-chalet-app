4bd2382afbf2e087598a3b21379cb2f2a1783f01
Magda Cembor <magda.cembor@gmail.com>
2020-02-28 15:44:50 +0000

    index pge

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,12 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, minZoom: 10, duration: 0 });
-  };
 
   const markers = JSON.parse(mapElement.dataset.markers);
 
@@ -14,8 +15,8 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      center: [ markers[0].lng, markers[0].lat ],
-      zoom: 12,
+      // center: [ markers[0].lng, markers[0].lat ],
+      // zoom: 12,
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     markers.forEach((marker) => {
@@ -23,7 +24,8 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
-    fitMapToMarkers(map, markers);
+  fitMapToMarkers(map, markers);
+
   }
 };
 
